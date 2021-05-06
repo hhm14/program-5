058fc220489ccb79c7407435d28982afe911f176
hhm14 <66333988+hhm14@users.noreply.github.com>
2021-05-06 18:42:15 -0500

    Create pa5.cpp

---
+++ pa5.cpp
@@ -0,0 +1,61 @@
+class Solution
+{
+public:
+
+    int recurse(vector<vector<int>>& graph, bool visited1[], bool visited2[], int n) { //bfs for CC count
+        if (graph[n].size() >= 1) {
+            visited1[n] = true;       
+            for (int i = 0; i < graph[n].size(); ++i) {
+                if (visited2[graph[n][i]] == false) {
+                    recurse(graph, visited2, visited1, graph[n][i]);
+                }
+            }
+            return 0;
+        }
+        
+        else {
+            visited1[n] = true;
+            return 0;
+        }
+        
+        
+    }
+
+    bool isBipartite(vector<vector<int>>& graph)
+    {
+
+
+        bool Gcheck[286] = {false};
+        bool GprimeCheckL[286]= {false};
+        bool GprimeCheckR[286]= {false};
+        
+        int G = 0;
+        int Gprime = 0; 
+        
+        for (int i = 0; i < graph.size(); ++i) {
+            if (GprimeCheckL[i] == false) 
+            {
+                recurse(graph, GprimeCheckL, GprimeCheckR, i);
+                Gprime++;     // track CC in G'
+            }
+            if (GprimeCheckR[i] == false) {
+                recurse(graph, GprimeCheckR, GprimeCheckL, i);
+                Gprime++;     // track CC in G'
+            }
+        }
+
+        for (int i = 0; i < graph.size(); ++i) {
+            if (Gcheck[i] == false) {
+                recurse(graph, Gcheck, Gcheck, i);
+                G++;          // track CC in G
+            }
+        }
+        
+        if (Gprime == 2*G) {
+            //theory 1
+            return true;
+        }  
+        
+        return false;
+    }
+};
